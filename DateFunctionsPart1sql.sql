eb02e391d32833dbdfe8bbc7822defc05351ea2e
Gabriel Arcanjo <gabrielarcanjo605@gmail.com>
2026-09-03 15:49:53 -0300

    Date Functions

---
+++ DateFunctionsPart1sql.sql
@@ -0,0 +1,74 @@
+--Number Functions
+--ROUND: above 5, round up, below 5 round down.
+SELECT 
+3.516,
+ROUND(3.516, 2) as ROUND_2,
+ROUND(3.516, 1) as ROUND_1,
+ROUND(3.516, 0) as ROUND_0
+
+--ABS/ABSOLUTE: Converts any negative number to positive.
+SELECT
+-10,
+ABS(-10),
+ABS(10)
+
+--Date and time functions
+SELECT	
+OrderID,
+OrderDate,
+ShipDate,
+CreationTime
+FROM Sales.Orders
+
+--Date and Time Values
+SELECT	
+OrderID,
+CreationTime,
+'2025-08-20' AS HardCoded,
+GETDATE() Today
+FROM Sales.Orders
+
+--Date and Time Functions and Extraction
+SELECT	
+OrderID,
+CreationTime,
+YEAR(CreationTime) AS Year,
+MONTH(CreationTime) AS Month,
+DAY(CreationTime) AS Day
+FROM Sales.Orders
+
+--DATEPART
+SELECT	
+OrderID,
+CreationTime,
+DATEPART(year, OrderDate) Year,
+DATEPART(month, OrderDate) Month,
+DATEPART(day, OrderDate) Day,
+DATEPART(hour, CreationTime) Hour,
+DATEPART(quarter, CreationTime) Quarter,
+DATEPART(week, CreationTime) Week
+FROM Sales.Orders
+
+--DATENAME
+SELECT
+OrderID,
+CreationTime,
+DATENAME(month, OrderDate) Month,
+DATENAME(weekday, OrderDate) Day
+FROM Sales.Orders
+
+--DATETRUNC
+SELECT
+OrderID,
+CreationTime,
+DATETRUNC(minute, CreationTime) Minute,
+DATETRUNC(day, CreationTime) day,
+DATETRUNC(year, CreationTime) year
+FROM Sales.Orders
+
+--EOMONTH
+SELECT 
+OrderID,
+CreationTime,
+EOMONTH(CreationTime) EndOfMonth
+FROM Sales.Orders
